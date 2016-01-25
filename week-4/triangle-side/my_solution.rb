97902f23e8de41e6437c88fd7d6f8d5bcaa911f9
Alex Williams <Tendoncs@gmail.com>
2016-01-25 09:11:34 -0800

    create PP files and solution

---
+++ week-4/triangle-side/my_solution.rb
@@ -0,0 +1,16 @@
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def valid_triangle?(a, b, c)
+  # Your code goes here!
+  if a+b>c && b+c>a && a+c>b
+return true
+else
+  return false
+end
+
+
+
+end
