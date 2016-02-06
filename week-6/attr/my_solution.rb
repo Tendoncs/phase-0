5cc3bed41763d8879b80ad31c542b923b64b8012
Alex Williams <Tendoncs@gmail.com>
2016-02-06 11:56:28 -0800

    setting up files

---
+++ week-6/attr/my_solution.rb
@@ -0,0 +1,41 @@
+#Attr Methods
+
+# I worked on this challenge [by myself, with:]
+
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+class NameData
+
+end
+
+
+class Greetings
+
+end
+
+
+
+# Reflection
+
+#answer:
+#question:
+
+
+#question: What are these methods doing?
+#answer: They are assigning or printing properties of class profile.
+#question: How are they modifying or returning the value of instance variables?
+#answer: by calling into the method with an input to change the class variables. Or by asking for the instance variables.
+
+
+#question: What changed between the last release and this release?
+#answer: change things around with commits and added a symbol changed names of methods.
+#question: What was replaced?
+#answer:nothing really just names of things.
+#question: Is this code simpler than the last?
+#answer:it was the same for me.
